92eb385d22e078562661412a3efc913a50fe3279
Aladine <alazaiane@yahoo.fr>
2014-04-08 14:08:47 +0200

    la bataille commence

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bdt270::Application.config.secret_key_base = 'c4ac68401a24347473e93b9c49874126ada1ead512aa3ae1f7914d84b92abf7f8ba69f7c69ee8a5807cb42d4557fb7672df7b70b3e79eb913f4707e204544422'
